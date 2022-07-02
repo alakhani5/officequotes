0176f6887f5ead969241d2f0d24ac96e7fc339c1
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-07-02 09:10:21 -0400

    setting up file system

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// setting up for api routing to the office quotes api
+
+// need to set up main port, listening, and middleware for logging and parsing json
---
+++ public/Main.js
@@ -0,0 +1,3 @@
+// main file for react js
+
+// need to export the jsx so it can be picked up by index.js and bundled correctly
